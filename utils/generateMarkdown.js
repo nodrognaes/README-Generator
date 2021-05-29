f4d84fc22d9caccb5bca243fd6056822d19eefc5
Sean Gordon <nodrog.naes@gmail.com>
2021-05-28 23:00:13 -0400

    js updated

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,33 +1,48 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license !== "None") {
-    return `![Github Badge](https://img.shields.io/badge/license-${data.license}-blue.svg)`
-  }
-  return "";
-}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {
+//   if (license !== "APACHE") {
+//     return `![Github Badge](https://img.shields.io/badge/license-Apache%202.0-blue.svg)`
+//   }
+//   else if (license !== "BOOST") {
+//     return `![Github Badge](https://img.shields.io/badge/license-Boost%201.0-blue.svg)`
+//   }
+//   else if (license !== "ECLIPSE") {
+//     return `![Github Badge](https://img.shields.io/badge/license-EPL%201.0-blue.svg)`
+//   }
+//   else if (license !== "MOZILLA") {
+//     return `![Github Badge](https://img.shields.io/badge/license-MPL%202.0-blue.svg)`
+//   }
+//   else if (license !== "None") {
+//     return `![Github Badge](https://img.shields.io/badge/license-${data.licenses}-blue.svg)`
+//   }
+//   else {
+//     return "";
+//   }
+// }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license !== "None") {
-    return `![Github License].`
-  }
-  return "";
-}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {
+//   if (license !== "None") {
+//     return `![Github License].`
+//   }
+//   return "";
+// }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license !== "None") {
-    return `![Github License].`
-  }
-  return "";
-}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {
+//   if (license !== "None") {
+//     return `![Github License].`
+//   }
+//   return "";
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.projTitle} #
+  ![Github Badge](https://img.shields.io/badge/license-${data.licenses}-blue.svg)
 
   ## Description: ##
   
@@ -52,7 +67,7 @@ function generateMarkdown(data) {
   
   ## <a name="license"></a>License: ##
   
-  ${data.licenses}
+  This application is covered under ${data.licenses}.
   
   ## <a name="contributing"></a>Contributing: ##
   
@@ -66,7 +81,7 @@ function generateMarkdown(data) {
   
   Github: [https://github.com/${data.github}](https://github.com/${data.github})
 
-  Email: ${data.email}
+  Please email any questions to: ${data.email}
 `;
 }
 
